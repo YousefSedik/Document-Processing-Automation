7636d32b6a63490765031f4ca52adb9122c54877
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-03-05 00:40:14 +0200

    feat: enhance admin interface for Document model with inline Category management

--- document/admin.py
+++ document/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Document, Category
+from django.contrib.admin import StackedInline
 
-admin.site.register(Document)
+class CategoryInline(admin.StackedInline):
+    model = Document.categories.through
+
+class DocumentAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+    
+admin.site.register(Document, DocumentAdmin)
 admin.site.register(Category)

7a8b13e3ce32ce4218aab247b7f73d4090d382cc
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-03-10 00:58:37 +0200

    feat: add IdentifyCategoryService module with Gemini and Ollama Llama3 integrations

---
+++ document/utils/IdentifyCategoryService/__init__.py
@@ -0,0 +1,7 @@
+from .IdentifyCategoryUsingGeminiAPI import IdentifyCategoryUsingGeminiAPI
+from .IdentifyCategoryUsingOllamaLlama3 import IdentifyCategoryUsingOllamaLlama3
+
+__all__ = [
+    "IdentifyCategoryUsingGeminiAPI",
+    "IdentifyCategoryUsingOllamaLlama3",
+]
